9767f24348fb183975e78016e5aae102ca3f91f5
samirahekmati <samirahekmati@gmail.com>
2024-10-25 18:01:41 +0100

    save element added

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ console.log(countEl);
 
 let count = 0; //initializer
 function increment(){
-    count = count +1;
+    count +=1;
     countEl.innerText = count;
 }
+let saveEl = document.getElementById("save-el");
 function save(){
-    console.log(count)
+    let countStr = count +" - ";
+    saveEl.textContent += countStr //innerText does not work very well here, so better we use textContent instead
+    countEl.textContent = 0 //set the number of passengers to 0 every time i press save
+    count = 0;
 }
-
 
 
 
